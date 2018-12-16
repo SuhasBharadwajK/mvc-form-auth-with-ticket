d53828db3e8c3dea0e54fbb5ce8c9bda4d33d580
Suhas Bharadwaj <suhas.five@gmail.com>
2018-12-16 17:09:41 +0530

    Initial commit

---
+++ MVC4FormsAuth.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using MVC4FormsAuth.Types.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4FormsAuth.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[AllowAnonymous]
+        [LogonAuthorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
